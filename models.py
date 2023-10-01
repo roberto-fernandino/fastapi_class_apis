a94c1c630f67eccd31b61a2b129c67e4057c0905
roberto-fernadino <romfernandino@gmail.com>
2023-10-01 10:55:03 -0300

    Models.py criado pra interagir com banco de dados.

---
+++ models.py
@@ -0,0 +1,25 @@
+from database import Base
+from sqlalchemy import Integer, String, Column
+from pydantic import BaseModel
+
+
+class Clientes(Base):
+    __tablename__ = 'clientes'
+    
+    id: Column(Integer, primary_key=True, index=True)
+    nombre: Column(String)
+    plato_favorito: Column(String) 
+
+class ClientesRequest(BaseModel):
+    nombre: str
+    plato_favorito: str
+
+    class Config:
+        json_schema_extra = {
+            'example': {
+                "nombre": "Nome_aqui",
+                "plato_favorito": "nome_do_prato"
+            }
+        }
+
+    
